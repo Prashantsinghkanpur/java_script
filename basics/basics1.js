8284ee63a0bd3a928ea8bb08f38f00dc14064dac
Prashant Singh <153474903+Prashantsinghkanpur@users.noreply.github.com>
2026-01-03 07:16:43 +0000

    datatypes

---
+++ basics/basics1.js
@@ -0,0 +1,12 @@
+// const accountId=1234;
+// let accountEmail="abc@gmail.com"
+// var accountPassword="1234"
+// accountCity="jaipur"
+// let accountState
+// /*
+// prefer nopt to use var 
+// because of issue in block scope abd functional scope
+// */
+// console.table([accountEmail,accountId,accountState,accountCity])
+console.log(typeof null)
+console.log(typeof undefined)
